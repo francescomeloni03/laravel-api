d2ef3ae66096a912995ef74e2e93e808d3c838e6
Francesco Meloni <francesco.meloni03@icloud.com>
2023-07-17 15:45:36 +0200

    C

---
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
+use App\Models\Project;
+use App\Http\Controllers\Controller;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Project::all();
+
+        return view("admin.projects.index", compact("projects"));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories= Category::all();
+        $technologys= Technology::all();
+
+        return view("admin.projects.create", compact("categories", "technologys"));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreNomeModelloRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProjectRequest $request)
+    {
+        $data = $request->validated();
+
+        $newProject = new Project();
+        $newProject->fill($data);
+        $newProject->save();
+
+        return to_route("admin.projects.show", $newProject);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\NomeModello  $nomeModello
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Project $project)
+    {
+        return view("admin.projects.show", compact("project"));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\NomeModello  $nomeModello
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(NomeModello $nomeModello)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateNomeModelloRequest  $request
+     * @param  \App\Models\NomeModello  $nomeModello
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateNomeModelloRequest $request, NomeModello $nomeModello)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\NomeModello  $nomeModello
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(NomeModello $nomeModello)
+    {
+        //
+    }
+}
